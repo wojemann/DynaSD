7d347b3288bfa02955239ba2a508662e1280fdc4
William Ojemann <wojemann@gmail.com>
2025-09-12 16:15:04 -0400

    removing small start

--- DynaSD/HFER.py
+++
@@ -1,9 +0,0 @@
-class HFER(DynaSDBase):
-    def __init__(self, w_size=1, w_stride=0.125):
-        super().__init__(w_size=w_size, w_stride=w_stride)
-
-    def fit(self, X):
-        self.X = X
-
-    def forward(self, X):
-        return self.X
